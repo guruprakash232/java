362f08313beedbe2cb1eabd81d97a9b500a006f4
Guruprakash <161027838+guruprakash232@users.noreply.github.com>
2024-11-15 12:06:50 +0530

    Add files via upload

---
+++ SampleTest.java
@@ -0,0 +1,21 @@
+//RETURN TYPE
+
+package javatask;
+public class SampleTest { 
+    static int addNumbers() {
+        int a=10;
+        int b=20;
+        int c=a+b;
+        
+        return c;
+        
+    }
+
+    public static void main(String[] args) { 
+    
+    	int c=addNumbers();
+    	
+    	System.out.println("The answer is"+" "+c);
+       
+}
+}
