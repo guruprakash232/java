2e8c10e4e9a34765930977821b503a649332bf09
Guruprakash <161027838+guruprakash232@users.noreply.github.com>
2024-11-19 16:58:17 +0530

    Add files via upload

---
+++ ArmstrongNUmber.java
@@ -0,0 +1,30 @@
+package javatask;
+import java.util.*;
+public class ArmstrongNUmber {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+      Scanner obj=new Scanner(System.in);
+      System.out.println("Enter the value");
+      int n=obj.nextInt();
+      int n1=n; 
+      int l,result=0;
+      while(n>0)
+      {
+    	 
+    	  l=n%10;
+    	  
+    	  result+=l*l*l;//result=l*l*l+result;
+    	 
+    	  n=n/10;
+    	  
+      }
+      if(n1==result)
+    	  System.out.println("The given number is armstrong number");
+      else
+    	  System.out.println("The given number is not a armstrong number");
+	}
+	
+
+}
