f0f8adabbe6ec0ab637f8cf9c727d4b3d0ceac79
Guruprakash <161027838+guruprakash232@users.noreply.github.com>
2024-11-19 12:33:17 +0530

    Add files via upload

---
+++ WhileLoopTask.java
@@ -0,0 +1,21 @@
+
+//write a program to print 10,20,30,40,50,60,70,80,90,100 using while loop
+
+
+package javatask;
+
+public class WhileLoopTask {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 10;
+        while (i <= 100) 
+         {
+        	System.out.println(i);
+            i += 10;
+            
+         }
+        
+	}
+
+}
