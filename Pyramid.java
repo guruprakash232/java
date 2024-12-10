c6a65112c43589ad49fa7db94c199f565792659b
Guruprakash <161027838+guruprakash232@users.noreply.github.com>
2024-12-10 17:26:59 +0530

    Add files via upload

---
+++ Pyramid.java
@@ -0,0 +1,21 @@
+package javatask;
+public class Pyramid {
+
+	public static void main(String[] args) {
+		int sum=0;
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				
+				 sum=sum+1;
+				System.out.print(sum+" ");
+				
+			}
+			System.out.println();
+		}
+	
+
+	}
+
+}
