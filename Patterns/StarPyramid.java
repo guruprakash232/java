341626e7b1a5d914cd9360173582911d3702d94f
Guruprakash <161027838+guruprakash232@users.noreply.github.com>
2024-12-13 12:07:42 +0530

    Add files via upload

---
+++ Patterns/StarPyramid.java
@@ -0,0 +1,36 @@
+//write a java program to print star pyramid 
+
+package javatask;
+
+public class StarPyramid {
+	
+	public static void main(String[] args) {
+		
+		int star=5;
+		
+		for(int i=1;i<=star;i++) {
+			
+			for(int j=star;j>=i;j--) 
+			{
+				
+				System.out.print(" ");
+			}
+			
+			for(int k=1;k<=i;k++)
+			{
+				System.out.print("*");
+				int n=2;
+				if(n<=k)
+            {
+				System.out.print("*");
+				
+			}
+					
+				}
+				
+				System.out.println("");
+			}
+		
+		}
+	}
+
