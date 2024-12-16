4b24429144c0517feded1f4a5bf37c19f8417e37
Guruprakash <161027838+guruprakash232@users.noreply.github.com>
2024-12-16 10:23:58 +0530

    Add files via upload

---
+++ Matrix/SubtractionMatrix.java
@@ -0,0 +1,58 @@
+package javatask;
+
+import java.util.*;
+
+public class SubtractionMatrix {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+        int row, col;
+
+        System.out.println("Enter the Number of Rows for the Matrix: ");// Input Rows
+        row = sc.nextInt();
+        
+        System.out.println("Enter the Number of Columns for the Matrix: ");// Input Columns
+        col = sc.nextInt();
+
+        
+        int a[][] = new int[row][col];// Declare Matrix
+        int b[][] = new int[row][col];// Declare Matrix
+        int c[][] = new int[row][col];// Declare Matrix
+
+// Input for Matrix 1
+        
+        System.out.println("Enter the elements of Matrix 1: ");
+        for (int i = 0; i < row; i++) { 
+            for (int j = 0; j < col; j++) { 
+                System.out.print("A[" + i + "][" + j + "]: ");
+                a[i][j] = sc.nextInt();
+            }
+        }
+
+// Input for Matrix 2
+        
+        System.out.println("Enter the elements of Matrix 2: ");
+        for (int i = 0; i < row; i++) { 
+            for (int j = 0; j < col; j++) {
+                System.out.print("B[" + i + "][" + j + "]: ");
+                b[i][j] = sc.nextInt();
+            }
+        }
+        
+// Perform matrix Subtraction
+        
+        System.out.println("\nMatrix Subtraction:\n");
+		for(int i = 0; i < row; i++) {
+	
+			for(int j = 0; j < col; j++) {
+		
+				c[i][j] = a[i][j] - b[i][j];
+				System.out.print(c[i][j] + " ");
+			
+			}
+		
+			System.out.println();
+		}sc.close();
+	}
+}
